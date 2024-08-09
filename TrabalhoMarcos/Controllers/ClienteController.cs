65f94d969f31b4f4ca96b627f3e051cf577ffd85
joseantonionunes <joseantonionunes2018@gmail.com>
2024-08-09 16:25:39 -0300

    Criando Cliente

---
+++ TrabalhoMarcos/Controllers/ClienteController.cs
@@ -0,0 +1,55 @@
+﻿using TrabalhoMarcos.Data;
+using TrabalhoMarcos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace Aula2407.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        private readonly AulaContext _context;
+
+        public ClienteController(AulaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> BuscarClientes()
+        {
+            return View(await _context.Clientes.ToListAsync());
+        }
+
+        public async Task<IActionResult> DetalhesClientes(int id)
+        {
+            return View(await _context.Clientes.FindAsync(id));
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AlterarCliente(int id)
+        {
+            return View(await _context.Clientes.FindAsync(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CadastroCliente([Bind("Id,Nome,RG,CPF,Usuario,Senha,CEP,UF,Cidade,Bairro,Rua,Numero,Completo")]  Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("BuscarClientes");
+            }
+            
+            return View(cliente);
+        }
+
+        
+    }
+}
